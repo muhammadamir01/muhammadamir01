719a33e74fc60bb152c78682bdca13a6842e5ac8
muhammadamir01 <muhammad.amir@netsoltech.com>
2022-06-28 10:10:25 +0500

    Hello

---
+++ HealthCheckControlle1 - Copy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NFS.API.UNITY.Controllers
+{
+    public class HealthCheckController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult Get()
+        {
+            return this.Json("API is up...");
+        }
+    }
+}
